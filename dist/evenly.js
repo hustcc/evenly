4ea23a575fbdb05f428beb32f2d784ca2a309a14
hustcc <i@atool.org>
2017-03-02 10:53:40 +0800

    add website demo

--- dist/evenly.js
+++ dist/evenly.js
@@ -1,31 +1,41 @@
 /**
  * Copyright (c) 2017 hustcc
  * License: MIT
- * Version: v1.0.1
+ * Version: v1.0.2
  * GitHub: https://github.com/hustcc/evenly
 **/
 
-/**
- *  evenly( gold, robber, fixed ) -> Array
- *  - gold (Number): the number of gold.
- *  - robber(Number): the number of robber who will divide the gold.
- *  - fixed (Number): the decimal number length.
- *  
- *  return the array how divide the gold evenly
-**/
-module.exports = function(gold, robber, fixed) {
-  if (fixed === undefined) fixed = 2;
-
-  var pow10 = Math.pow(10, fixed);
-  gold *= pow10;
-
-  var divide = Math.floor(gold / robber),
-    remain = gold - divide * robber;
-  
-  var result = [], cnt = 0;
-  // 0 .. n-1 element
-  while(++ cnt && cnt <= robber) {
-    result.push((cnt <= remain ? divide + 1 : divide) / pow10);
+/* jshint expr: true */
+!function (root, factory) {
+  if (typeof module === 'object' && module.exports) {
+    module.exports = factory(root); // nodejs support
+    module.exports['default'] = module.exports; // es6 support
   }
-  return result;
-};
+  else
+    root.evenly = factory(root);
+}(typeof window !== 'undefined' ? window : this, function () {
+  /**
+   *  evenly( gold, robber, fixed ) -> Array
+   *  - gold (Number): the number of gold.
+   *  - robber(Number): the number of robber who will divide the gold.
+   *  - fixed (Number): the decimal number length.
+   *  
+   *  return the array how divide the gold evenly
+  **/
+  return function(gold, robber, fixed) {
+    if (fixed === undefined) fixed = 2;
+
+    var pow10 = Math.pow(10, fixed);
+    gold *= pow10;
+
+    var divide = Math.floor(gold / robber),
+      remain = gold - divide * robber;
+    
+    var result = [], cnt = 0;
+    // 0 .. n-1 element
+    while(++ cnt && cnt <= robber) {
+      result.push((cnt <= remain ? divide + 1 : divide) / pow10);
+    }
+    return result;
+  };
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 const gulp = require('gulp');
 const injectVersion = require('gulp-inject-version');
+const uglify = require('gulp-uglify');
+const rename = require("gulp-rename");
 
 gulp.task('build', function() {
   gulp.src('src/evenly.js')
   .pipe(injectVersion())
+  .pipe(gulp.dest('dist/'))
+  .pipe(uglify({
+    preserveComments: 'license'
+  }))    //uglify
+  .pipe(rename("evenly.min.js"))
   .pipe(gulp.dest('dist/'))
 });
